80b06d6e76f202d3a00fa76a8b052579ca5f7990
TaeYoung1217 <wkdxodud44@gmail.com>
2024-06-23 14:08:02 +0900

    🪛 구현 마무리 및 주석 추가

--- frontend/login.js
+++ frontend/login.js
@@ -2,17 +2,19 @@ const form = document.getElementById("login_form");
 
 const handleSubmit = async (event) => {
   event.preventDefault();
+
   const formData = new FormData(form);
-  const sha256password = sha256(formData.get("password"));
+  const sha256password = sha256(formData.get("password")); //패스워드 암호화
   formData.set("password", sha256password);
 
   const sendData = {
-    id: formData.get("id"),
-    password: formData.get("password"),
+    id: formData.get("id"), //form에 입력된 id 가져오기
+    password: formData.get("password"), //암호화된 패스워드 가져오기
   };
 
   const res = await fetch("/login", {
-    method: "POST",
+    //사용자 조회를 위해 POST 요청, 보안때문에 get을 안하고 post.
+    method: "POST", //post 요청이 body에 정보를 담아서 보내기때문에 get보다 보안성이 높다
     headers: {
       "Content-Type": "application/json",
     },
@@ -20,7 +22,9 @@ const handleSubmit = async (event) => {
   });
   const data = await res.json();
 
-  switch (data) {
+  switch (
+    data //서버에서 return 받으면
+  ) {
     case "invalid password":
       alert("비밀번호가 틀립니다");
       break;
@@ -28,8 +32,10 @@ const handleSubmit = async (event) => {
       alert("존재하지 않는 아이디");
       break;
     default:
+      const accessToken = data; //로그인 성공하면 accesstoken을 받아
+      window.localStorage.setItem("token", accessToken); //localstorage에 저장
       alert("로그인 성공");
-      window.location.pathname = "/";
+      window.location.pathname = "/"; //홈화면으로 이동
       break;
   }
 };
