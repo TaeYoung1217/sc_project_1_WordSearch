fff1f57841d9451989c38593aa30a8648c569e87
TaeYoung1217 <wkdxodud44@gmail.com>
2024-06-23 14:08:28 +0900

    🪛 로그아웃 기능 구현
    로컬스토리지에 저장된 토큰을 삭제하는 방식으로 로그아웃 구현

---
+++ frontend/logout.js
@@ -0,0 +1,10 @@
+const logout_button = document.getElementById("logout_button");
+
+const handleLogout = () => {
+  //로그아웃 버튼 누르면
+  window.localStorage.setItem("token", ""); //local storage에 저장된 token값 빈칸으로 설정
+  alert("로그아웃 되었습니다");
+  window.location.pathname = "index.html";
+};
+
+logout_button.addEventListener("click", handleLogout);
