ee9dc252897457d20bb389466558d2d01d270bee
TaeYoung1217 <wkdxodud44@gmail.com>
2024-06-23 14:08:53 +0900

    🪛 maker 기능 구현

--- frontend/maker.js
+++ frontend/maker.js
@@ -1,6 +1,5 @@
 import { WIDTH } from "./display.js";
 const form = document.getElementById("maker_form");
-let Answer = [];
 
 function isEnglish(text) {
   // 영어 소문자(a-z)와 대문자(A-Z)로만 구성되어 있는지 검사
@@ -8,41 +7,44 @@ function isEnglish(text) {
 }
 
 const handleSubmit = async (event) => {
+  //추가버튼 누르면
   event.preventDefault();
   const formData = new FormData(form);
   for (let i = 0; i < WIDTH; i++) {
+    //최대가 너비만큼의 단어 길이이기 때문에
     const div = document.getElementById(`error[${i}]`);
     const inputdiv = document.getElementById(`div[${i}]`);
     const inputText = formData.get(`input[${i}]`);
     if (isEnglish(inputText)) {
-      if (!Answer.includes(inputText)) {
-        if (inputText.length < 3) {
-          div.innerText = "3글자 이상 입력하세요";
-        } else Answer.push(inputText);
+      //영어단어인지 판단
+
+      const res = await fetch("/addWords", {
+        //서버에 post 요청, 서버에서는 중복 확인하여 DB에 저장
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ content: inputText }),
+      });
+      const data = await res.json();
+      if (data === "200") {
+        alert("추가 성공");
+        window.location.pathname = "/";
       }
+
       div.innerText = "";
     } else {
+      //오류 표시
       div.style.color = "red";
       div.innerText = "영어만 입력하세요";
 
       inputdiv.appendChild(div);
     }
   }
-
-  for (let i = 0; i < Answer.length; i++) {
-    const res = await fetch("/addWords", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ answer: Answer[i].toUpperCase() }),
-    });
-    console.log(JSON.stringify(Answer[i]));
-    const data = await res.json();
-  }
 };
 
 const makeInput = () => {
+  //입력창 만드는 반복문
   for (let i = 0; i < WIDTH; i++) {
     const div = document.createElement("div");
     div.id = `div[${i}]`;
