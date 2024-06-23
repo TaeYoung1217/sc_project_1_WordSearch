5918ea5f8ea539bca80a07bdf38062962d448e88
TaeYoung1217 <wkdxodud44@gmail.com>
2024-06-23 14:09:17 +0900

    🪛 오답표시 기능 구현
    animate를 이용하여 오답표시 기능 구현

--- frontend/playgame.js
+++ frontend/playgame.js
@@ -1,6 +1,5 @@
 import { ANSWER } from "./display.js";
-import { WIDTH } from "./display.js";
-import { HEIGHT } from "./display.js";
+import { WIDTH, HEIGHT, DIRECTION } from "./display.js";
 import { gameOver } from "./gameover.js";
 
 let isMouseDown = false;
@@ -10,15 +9,15 @@ let end_board = null;
 
 const handleMouseDown = (event) => {
   isMouseDown = true;
-  start_board = event.target;
-  highlightBoard(start_board);
+  start_board = event.target; //마우스 down이벤트가 발생한 보드
+  highlightBoard(start_board); //시작지점부터 마우스로 드래그하는 부분 색칠하기
 };
 
 const handleMouseOver = (event) => {
   if (isMouseDown) {
-    const cur_board = event.target;
-    highlightBoard(cur_board);
-    end_board = cur_board;
+    const cur_board = event.target; //드래그하면서 움직이는 동안 보드 저장
+    highlightBoard(cur_board); //색칠
+    end_board = cur_board; //끝나는 지점 저장
   }
 };
 
@@ -26,19 +25,39 @@ const handleMouseUp = (event) => {
   isMouseDown = false;
 
   if (start_board && end_board) {
+    //최대한 다른 요소까지 색칠되지 않게 하기 위해 board를 div안에 감싸고
+    //board에만 이벤트 리스너를 부착, 마우스로 보드를 드래그하면 보드를 감싸고 있는 부모 요소를 찾아
     if (start_board == document.querySelector(".board").parentElement)
       start_board = start_board.children;
     else if (end_board == document.querySelector(".board".parentElement))
       end_board = end_board.children;
     //단어확인 및 처리 로직 구현
     getSelectedWord(start_board, end_board);
-    isGameOver();
+    isGameOver(); //정답을 모두 맞추면 game over
   }
+};
 
-  clearSelction();
+const wrongAnswer = (axis, length) => {
+  for (let i = 0; i < length; i++) {
+    const wrongBoard = document.getElementById(
+      `board[${axis[i][1]}][${axis[i][0]}]`
+    );
+
+    wrongBoard.animate(
+      [
+        { transform: "translateY(0px)", background: "yellow" }, //시작위치
+        {
+          transform: "translateY(-10px)",
+          background: "yellow",
+        }, //도착위치
+      ],
+      { duration: 250, iterations: 2 } //지속시간, 반복횟수
+    );
+  }
 };
 
 const highlightAnswer = (axis, length) => {
+  //정답 색칠하는 부분
   for (let i = 0; i < length; i++) {
     const answerBoard = document.getElementById(
       `board[${axis[i][1]}][${axis[i][0]}]`
@@ -51,6 +70,7 @@ const highlightAnswer = (axis, length) => {
 };
 
 const highlightBoard = (board) => {
+  //드래그하는 부분 노랗게 색칠
   if (board.style.backgroundColor == "green") return;
   else {
     board.style.backgroundColor = "yellow";
@@ -59,10 +79,12 @@ const highlightBoard = (board) => {
 };
 
 const clearSelction = () => {
+  //전체 보드의 backgroundcolor 를 화이트로
   for (let i = 0; i < WIDTH; i++) {
     for (let j = 0; j < HEIGHT; j++) {
       const board = document.getElementById(`board[${i}][${j}]`);
       if (board.style.backgroundColor == "green") continue;
+      //정답이 색칠되어 있으면 넘어가기
       else {
         board.style.backgroundColor = "white";
         board.parentElement.style.backgroundColor = "white";
@@ -72,24 +94,28 @@ const clearSelction = () => {
 };
 
 const isGameOver = () => {
-  const answerList = document.getElementById("answer_list");
+  //게임 끝났는지 판단
+  const answerList = document.getElementById("answer_list"); //표시된 정답 list를 가져와서
   let check = true;
   for (let i = 0; i < answerList.childElementCount; i++) {
     const answer = document.getElementById(`answer_list${i}`);
     if (answer.style.textDecoration != "line-through") {
-      check = false;
-      break;
+      //가운데 줄 그어 있는지 확인, 아니라면 return
+      return;
     }
   }
-  if (check) gameOver();
+  gameOver(); //모든 정답에 정답표시 되어있으면 gameover 처리
 };
 
 const isAnswer = (word) => {
+  //드래그한 부분의 단어를 읽어들어와서 정답인지 판단
   for (let i = 0; i < ANSWER.length; i++) {
+    //DB에서 가져온 단어 리스트에 드래그한 단어가 있는지 확인
     const answer_list = document.getElementById(`answer_list${i}`);
     if (answer_list.innerText === word) {
-      answer_list.style.textDecoration = "line-through";
-      answer_list.style.color = "green";
+      //있으면 정답표시
+      answer_list.style.textDecoration = "line-through"; //가운데 줄 긋고
+      answer_list.style.color = "green"; //초록색으로 표시
       return true;
     }
   }
@@ -97,6 +123,7 @@ const isAnswer = (word) => {
 };
 
 const getWord = (j, i, word) => {
+  //마우스로 지가나는 보드의 텍스트 가져오기
   const cur_board = document.getElementById(`board[${j}][${i}]`);
   word = cur_board.innerText;
 
@@ -104,13 +131,14 @@ const getWord = (j, i, word) => {
 };
 
 const getSelectedWord = (start, end) => {
-  const start_row = start.dataset.row;
-  const start_col = start.dataset.col;
-  const end_row = end.dataset.row;
-  const end_col = end.dataset.col;
-  const dx = end_col - start_col;
-  const dy = end_row - start_row;
-  let axis = [];
+  //마우스로 선택한 단어 가져오기
+  const start_row = start.dataset.row; //시작점의 row
+  const start_col = start.dataset.col; //시작점의 col
+  const end_row = end.dataset.row; //도착점 row
+  const end_col = end.dataset.col; //도착점 col
+  const dx = end_col - start_col; //가로 변화량
+  const dy = end_row - start_row; //세로 변화량
+  let axis = []; //지나간 좌표 저장하기 위한 배열
   let i = start_col; //x 시작점
   let j = start_row; //y 시작점 (j,i)에서 시작 헷갈리지 않기!
   let word = "";
@@ -118,26 +146,32 @@ const getSelectedWord = (start, end) => {
   if (dx == 0 && dy > 0) {
     //아래
     while (true) {
-      axis.push([i, j]);
+      axis.push([i, j]); //마우스로 드래그한 부분 좌표 저장
       word += getWord(j, i, word);
-      console.log(word);
       j++;
-      if (j > end_row) break;
+      if (j > end_row) break; //진행 방향으로 가다가 도착지점까지 가면 반복문 탈출
     }
 
-    if (isAnswer(word)) highlightAnswer(axis, word.length);
+    if (isAnswer(word)) highlightAnswer(axis, word.length); //정답이라면 색칠
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
-    return word;
+    return word; //저장한 단어 반환
   } else if (dx == 0 && dy < 0) {
     //위
     while (true) {
       axis.push([i, j]);
       word += getWord(j, i, word);
-      console.log(word);
       j--;
       if (j < end_row) break;
     }
     if (isAnswer(word)) highlightAnswer(axis, word.length);
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
     return word;
   } else if (dx > 0 && dy == 0) {
@@ -145,11 +179,14 @@ const getSelectedWord = (start, end) => {
     while (true) {
       axis.push([i, j]);
       word += getWord(j, i, word);
-      console.log(word);
       i++;
       if (i > end_col) break;
     }
     if (isAnswer(word)) highlightAnswer(axis, word.length);
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
     return word;
   } else if (dx < 0 && dy == 0) {
@@ -157,11 +194,14 @@ const getSelectedWord = (start, end) => {
     while (true) {
       axis.push([i, j]);
       word += getWord(j, i, word);
-      console.log(word);
       i--;
       if (i < end_col) break;
     }
     if (isAnswer(word)) highlightAnswer(axis, word.length);
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
     return word;
   } else if (dx > 0 && dy < 0) {
@@ -169,12 +209,15 @@ const getSelectedWord = (start, end) => {
     while (true) {
       axis.push([i, j]);
       word += getWord(j, i, word);
-      console.log(word);
       i++;
       j--;
       if (i > end_col || j < end_row) break;
     }
     if (isAnswer(word)) highlightAnswer(axis, word.length);
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
     return word;
   } else if (dx > 0 && dy > 0) {
@@ -182,12 +225,15 @@ const getSelectedWord = (start, end) => {
     while (true) {
       axis.push([i, j]);
       word += getWord(j, i, word);
-      console.log(word);
       i++;
       j++;
       if (i > end_col || j > end_row) break;
     }
     if (isAnswer(word)) highlightAnswer(axis, word.length);
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
     return word;
   } else if (dx < 0 && dy < 0) {
@@ -195,12 +241,15 @@ const getSelectedWord = (start, end) => {
     while (true) {
       axis.push([i, j]);
       word += getWord(j, i, word);
-      console.log(word);
       i--;
       j--;
       if (i < end_col || j < end_row) break;
     }
     if (isAnswer(word)) highlightAnswer(axis, word.length);
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
     return word;
   } else if (dx < 0 && dy > 0) {
@@ -208,18 +257,22 @@ const getSelectedWord = (start, end) => {
     while (true) {
       axis.push([i, j]);
       word += getWord(j, i, word);
-      console.log(word);
       i--;
       j++;
       if (i < end_col || j > end_row) break;
     }
     if (isAnswer(word)) highlightAnswer(axis, word.length);
+    else {
+      wrongAnswer(axis, word.length);
+      clearSelction();
+    }
 
     return word;
   }
 };
 
 export function addListener(board) {
+  //이벤트 리스너 부착
   board.addEventListener("mousedown", handleMouseDown);
   board.addEventListener("mouseover", handleMouseOver);
   board.addEventListener("mouseup", handleMouseUp);
